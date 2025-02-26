961215090f4ebc0831e0f34ca1c537b2ff9e13c1
Mr-Eke <c.eke@alustudent.com>
2025-02-26 11:03:53 +0200

    client configuration file with puppet

--- ssh/100-puppet_ssh_config.pp
+++ ssh/100-puppet_ssh_config.pp
@@ -1,13 +1,13 @@
 # reconfigure clients config file with puppet
 
-file_line { 'disable_password_auth':
-  ensure => present,
+file_line { 'Turn off passwd auth':
+  ensure => 'present',
   path   => '/etc/ssh/ssh_config',
   line   => '    PasswordAuthentication no',
 }
 
-file_line { 'set_identity_file':
-  ensure => present,
+file_line { 'Declare identity file':
+  ensure => 'present',
   path   => '/etc/ssh/ssh_config',
-  line   => '    IdentityFile ~/.ssh/id_rsa_alu',
+  line   => '    IdentityFile ~/.ssh/school',
 }
